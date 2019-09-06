091394e15eb423eb04691566ac2d785be787d12e
Selhar <selhar@protonmail.com>
2019-09-06 10:31:17 -0300

    feat: added colors styleguide file.

---
+++ styleguide/colors.js
@@ -0,0 +1,10 @@
+// color definitions
+export const yellow = "yellow"
+export const red = "red"
+export const black = "black"
+
+// semantic colors
+export const primary = yellow;
+export const secondary = red;
+export const transparentBackground = "rgba(3, 3, 3, 0.6)"
+
