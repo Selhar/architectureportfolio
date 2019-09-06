f94bdeffd12cc83cd95937d1fd96627ab84d3393
Selhar <selhar@protonmail.com>
2019-09-06 10:31:08 -0300

    feat: added reset.js

---
+++ styleguide/reset.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  body {
+    margin: 0;
+    border: 0;
+    box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,Helvetica, sans-serif;
+  }
+`
