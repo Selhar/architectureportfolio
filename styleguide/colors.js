eb6d788e47f375fe6ff5134df900214d915e3904
Selhar <selhar@protonmail.com>
2019-09-14 05:14:43 -0300

    feat: added temporary splash image, replaced colors and reset files.

--- styleguide/colors.js
+++ styleguide/colors.js
@@ -1,10 +1,12 @@
 // color definitions
-export const yellow = "yellow"
+export const yellow = "#fff317"
 export const red = "red"
 export const black = "black"
+export const grey = "#111"
 
 // semantic colors
 export const primary = yellow;
 export const secondary = red;
-export const transparentBackground = "rgba(3, 3, 3, 0.6)"
+export const transparentBackground = "rgba(3, 3, 3, 0.8)"
+export const hardBackground = grey;
 
--- styleguide/reset.js
+++ styleguide/reset.js
@@ -1,10 +1,22 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
-  body {
+  body{
+    font-family: 'Lato', sans-serif;
+    max-width: 1600px;
+    margin: 0 auto;
+    background-color: #fff317;
+    & * {
+      margin: 0;
+      border: 0;
+      padding: 0;
+      text-decoration: none;
+      box-sizing: border-box;
+    }
+  }
+
+  p {
     margin: 0;
-    border: 0;
-    box-sizing: border-box;
-    font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,Helvetica, sans-serif;
+    padding: 0;
   }
 `
