d9945f32d4fb7364e3b49bbc0fe12b4a63a3e1ff
Selhar <selhar@protonmail.com>
2019-10-11 22:28:48 -0300

    chore: added custom fonts.

--- styleguide/reset.js
+++ styleguide/reset.js
@@ -1,10 +1,25 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+  @font-face {
+      font-family: 'Geogrotesque';
+      src: url('/static/fonts/Geogtq-Md.otf');
+  }
+
+  @font-face {
+      font-family: 'Sensation Regular';
+      src: url('/static/fonts/sansation_regular.ttf');
+  }
+
+  @font-face {
+      font-family: 'Sensation Bold';
+      src: url('/static/fonts/sansation_bold.ttf');
+  }
+
   body{
     max-width: 1600px;
     margin: 0 auto;
-    font-family: 'Montserrat', sans-serif;
+    font-family: 'Sensation Regular', sans-serif;
     & * {
       margin: 0;
       border: 0;
