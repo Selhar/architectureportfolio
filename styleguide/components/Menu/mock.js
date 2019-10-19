28b17ac2ac3671529372126c4502e726b4052c88
Selhar <selhar@protonmail.com>
2019-10-18 23:41:46 -0300

    chore: added facebook and whatsapp icons. Removed unecessary files.

--- styleguide/components/Menu/mock.js
+++
@@ -1,3 +0,0 @@
-export default {
-  menu: ['1','2','3']
-}
--- styleguide/components/Social/stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Social from './'
-
-export default {
-  title: 'Social',
-};
-
-export const Basic = () => <Social />
