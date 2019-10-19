4cf57736c6d51620519932de22bfccf6a9dc8e09
Selhar <selhar@protonmail.com>
2019-10-19 01:55:11 -0300

    chore: added correct data.

--- styleguide/data.js
+++ styleguide/data.js
@@ -1,5 +1,5 @@
 export const email = "thaisacramento@yahoo.com"
 export const mensagemSplash = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has be"
 export const mensagemEstudio = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It"
-export const facebook = "http://www.google.com"
-export const whatsapp = "http://www.google.com"
+export const instagram = "https://www.instagram.com/tsmodelagem3d/"
+export const whatsapp = "https://wa.me/5571999019845"
