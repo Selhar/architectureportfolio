fd04047f9ef7f9877f551dd05f5e96e64d68c202
Selhar <selhar@protonmail.com>
2019-11-18 04:27:37 -0300

    chore: add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -30,6 +30,7 @@ class MyDocument extends Document {
             name="viewport"
             content="width=device-width, initial-scale=1, user-scalable=no"
           />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
           <link rel="icon" type="image/png" href="/static/favicon.png" />
           <link
             href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600,800&display=swap"
