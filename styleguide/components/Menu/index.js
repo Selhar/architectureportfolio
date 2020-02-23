1b9fccba8296b777e7fa089acb51a545674e4a78
Selhar <selhar@protonmail.com>
2020-02-23 14:25:55 -0300

    added colors, theme and config

--- styleguide/components/Menu/index.js
+++ styleguide/components/Menu/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 export default ({items}) => (
-  <ul className="uppercase flex flex-row transform -translate-x-16">
+  <ul className="uppercase flex flex-row transform -translate-x-24 tracking-wider">
     {
       items.map(item => (
-        <li key={item} className="mr-4">{item}</li>
+        <li key={item} className="mr-4 font-bold">{item}</li>
       ))
     }
   </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,34 @@
 module.exports = {
   theme: {
-    extend: {}
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+    },
+    borderWidth: {
+      default: '1px',
+      '0': '0',
+      '2': '2px',
+      '4': '4px',
+    },
+    extend: {
+      colors: {
+        grey: '#878787',
+        yellow: '#ffc100',
+        orange: '#ff6300',
+        lightOrange: '#ff8b00',
+        black: '#111',
+      },
+      spacing: {
+        '96': '24rem',
+        '128': '32rem',
+      }
+    }
   },
   variants: {},
   plugins: []
