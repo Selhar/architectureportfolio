d3c71a7cb052f8bbc9e83aa5fa93709ea9780bf5
Selhar <selhar@protonmail.com>
2020-04-22 06:59:08 -0300

    chore: added technical debt file; adapted tailwind colors; added mock data

--- styleguide/data.js
+++ styleguide/data.js
@@ -3,4 +3,38 @@ export const mensagemSplash = "Lorem Ipsum is simply dummy text of the printing 
 export const mensagemEstudio = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It";
 export const instagram = "https://www.instagram.com/tsmodelagem3d/";
 export const whatsapp = "https://wa.me/5571999019845";
-export const menuItems = ["Home", "Sobre", "Portfolio", "Orçamento", "Contato"];
+export const menuItems = [["Home", "#"], ["Sobre","#about"], ["Portfolio", "#portfolio"], ["Orçamento", "#orcamento"], ["Contato", "#contact"]];
+export const phone = "+55 (71) 9 9901-9845";
+export const perfis = [
+  {
+    id: "architect",
+    label: "Arquiteto(a)"
+  },
+  {
+    id: "entrepeneur",
+    label: "Construtora/Empreendedor(a)"
+  },
+  {
+    id: "designer",
+    label: "Decorador(a)"
+  }
+];
+
+export const projectType = [
+  {
+    id: "illustrative",
+    label: "Imagem ilustrativa"
+  },
+  {
+    id: "animationmodel",
+    label: "Modelagem em 3d"
+  },
+  {
+    id: "videomodel",
+    label: "Animação/vídeo"
+  },
+  {
+    id: "image",
+    label: "Imagens 360º"
+  }
+];
