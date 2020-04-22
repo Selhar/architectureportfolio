15e5f2d6851f52a1a469828431ea0b64762cf02a
Selhar <selhar@protonmail.com>
2020-04-22 06:57:21 -0300

    chore: install and configure new packages

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  plugins: [require("tailwindcss"), require("postcss-preset-env")]
+  plugins: [
+    require("tailwindcss"), 
+    require('autoprefixer'),
+    require("postcss-preset-env"),
+    // require("@fullhuman/postcss-purgecss")({
+    //   content: [
+    //     './styleguide/components/**/*.js',
+    //     './styleguide/components/**/*.css',
+    //     './pages/*.js'
+    //   ]
+    // })
+  ]
 };
