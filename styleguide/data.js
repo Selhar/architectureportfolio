5913de296c7f7b34d92d06f653e76540ec9cdb30
Selhar <selhar@protonmail.com>
2020-07-11 15:50:30 -0300

    chore: update site data

--- styleguide/data.js
+++ styleguide/data.js
@@ -1,4 +1,4 @@
-export const email = "thaisacramento@yahoo.com";
+export const email = "tsmodelagem3d@gmail.com";
 export const mensagemSplash = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has be";
 export const mensagemEstudio = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It";
 export const instagram = "https://www.instagram.com/tsmodelagem3d/";
