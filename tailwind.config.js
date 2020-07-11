55fe470924a5538f3232ec4acf58c5e942a7531f
Selhar <selhar@protonmail.com>
2020-07-11 15:49:44 -0300

    chore: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,15 @@ module.exports = {
       md: '768px',
       lg: '1024px',
       xl: '1280px',
+      xxl: '1600px',
     },
     fontFamily: {
       display: ['Gilroy', 'sans-serif'],
       body: ['Graphik', 'sans-serif'],
+    },
+    borderRadius: {
+      'md': '0.25rem',
+      'xl': '3rem'
     },
     borderWidth: {
       default: '1px',
@@ -16,13 +21,26 @@ module.exports = {
       '2': '2px',
       '4': '4px',
     },
+    backgroundColor: {
+      black:        '#111',
+      grey:         '#222',
+      lightGrey:    '#333',
+      orange:       '#955d21',
+      yellow:       '#ffc100'
+    },
+    fill: {
+      'orange': '#955d21',
+      'white': 'white',
+      'yellow': '#ffc100'
+    },
+    placeholder: {
+      'grey': '#ffc100',
+    },
     extend: {
       colors: {
-        grey: '#878787',
-        yellow: '#ffc100',
-        orange: '#ff6300',
-        lightOrange: '#ff8b00',
-        black: '#111',
+        yellow:       '#ffc100',
+        orange:       '#955d21',
+        lightOrange:  '#FDC582',
       },
       spacing: {
         '96': '24rem',
@@ -30,6 +48,8 @@ module.exports = {
       }
     }
   },
-  variants: {},
+  variants: {
+    fill: ['responsive, hover']
+  },
   plugins: []
 }
