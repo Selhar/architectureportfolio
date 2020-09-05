3b695a8ae298e11aaf5c71b33835593767e3bfd8
Selhar <selhar@protonmail.com>
2020-09-05 08:19:21 -0300

    feat: added tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,2 +1,14 @@
-const Home = () => <h1>oi</h1>;
-export default Home;
+import Nav from "../components/nav";
+
+export default function IndexPage() {
+  return (
+    <div>
+      <Nav />
+      <div className="py-20">
+        <h1 className="text-5xl text-center text-accent-1">
+          Next.js + Tailwind CSS
+        </h1>
+      </div>
+    </div>
+  );
+}
