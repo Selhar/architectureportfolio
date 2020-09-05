59a30aafc3831d48447967c7df0a97c035326547
Selhar <selhar@protonmail.com>
2020-09-05 10:31:19 -0300

    chore: add default font and relevant static files

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,35 @@ module.exports = {
   },
   purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      body: [
+        "Geogrotesque",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Segoe UI",
+        "Roboto",
+        "Oxygen",
+      ],
+      serif: [
+        "Geogrotesque",
+        "Sensation Regular",
+        "Sensation Bold",
+        "Georgia",
+        "Cambria",
+        "Times New Roman",
+        "Times",
+      ],
+      mono: [
+        "Geogrotesque",
+        "Sensation Regular",
+        "Sensation Bold",
+        "Menlo",
+        "Monaco",
+        "Consolas",
+        "Liberation Mono",
+        "Courier New",
+      ],
+    },
     extend: {
       colors: {
         yellow: "#ffc100",
