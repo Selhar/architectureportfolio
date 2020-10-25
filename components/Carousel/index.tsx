d9c8720f2aa5949b081288bc80f0bfb46188f7a6
Selhar <selhar@protonmail.com>
2020-10-25 20:48:12 -0300

    feat: new carousel strategy

--- components/Carousel/index.tsx
+++ components/Carousel/index.tsx
@@ -1,42 +1,28 @@
+
 import React, { useState, useEffect } from "react";
 
-import { CarouselImage } from "../utils/constants";
+import { Image } from "../utils/types";
+import Splash from "../Splash"
+import { orderImages } from "../utils/funcs"
 
 type Carousel = {
-  images: Array<CarouselImage>;
-  galleryMode?: boolean;
+  images: Array<Image>;
 };
-
-const processCurrentImageClass = (value: number) => {
-  switch(value) {
-    case 0:
-      return "translate-x-0";
-    case 100:
-      return "translate-x-100";
-    case 200:
-      return "translate-x-200";
-    case 300:
-      return "translate-x-300";
-    case 400:
-      return "translate-x-400";
-    case 500:
-      return "translate-x-500";
-    case 600:
-      return "translate-x-600";
-  }
-}
 
 const Carousel = ({ images }: Carousel) => {
   const [currentImage, setcurrentImage] = useState(0);
-  const [currentImageClass, setCurrentImageClass] = useState(processCurrentImageClass(currentImage));
+  const [isTransitioning, setIsTransitioning] = useState("transform -translate-x-100");
   const AUTOSCROLLING_DURATION = 5000;
+  let isLastImage = currentImage + 1 >= images.length
 
   useEffect(() => {
-    let timer = setInterval(
-      () =>
-        {setcurrentImage(
-          currentImage == images.length * 100 - 100 ? 0 : currentImage + 100
-        ), setCurrentImageClass("translate-x-"+currentImage)},
+    let timer = setInterval(() => {
+      setIsTransitioning("transition-transform duration-500 ease-carousel transform -translate-x-200");
+      setTimeout(() => {
+        setcurrentImage(isLastImage ? 0 : currentImage + 1);
+        setIsTransitioning("transform -translate-x-100");
+      }, 500);
+    },
       AUTOSCROLLING_DURATION
     );
     return () => clearInterval(timer);
@@ -44,32 +30,12 @@ const Carousel = ({ images }: Carousel) => {
 
   return (
     <div
-      className={`flex transition duration-500 
-        all ease-in-out transform ${currentImageClass}`}
+    className={`flex ${isTransitioning}`}
     >
-      {images.map((image) => (
-        <div
-          key={"carousel-" + image.name}
-          className={"flex-shrink-0 w-full m-auto"}
-        >
-          <picture>
-            <source
-              media="(min-width: 1024px) and (max-width:2559px)"
-              srcSet={image["1080p"]}
-            />
-            <source
-              media="(min-width: 2560px) and (max-width:4095px)"
-              srcSet={image["1440p"]}
-            />
-            <source media="(min-width:4095px)" srcSet={image["2160p"]} />
-            <img
-              className={
-                "flex-grow-0 flex-shrink-0 max-w-screen w-full max-w-screen h-auto max-h-screen m-auto "
-              }
-              src={image.mobile}
-            />
-          </picture>
-        </div>
+      {images.map((image, index) => (
+        <Splash image={image}
+          classes={`flex-shrink-0 ${orderImages(index, currentImage, isLastImage, images.length)}` }
+        />
       ))}
     </div>
   );
