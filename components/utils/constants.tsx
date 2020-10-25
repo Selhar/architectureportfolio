95d5a8562af06d967a74f609b4e5d2d5d507eae2
Selhar <selhar@protonmail.com>
2020-10-25 20:50:41 -0300

    feat: import all project images

--- components/utils/constants.tsx
+++ components/utils/constants.tsx
@@ -1,7 +1,184 @@
+
+export const PortfolioImages = {
+  1: {
+    name: "banheiro",
+    description: "descrição",
+    "2160p": "/portfolio/banheiro/2160p.jpg",
+    "1440p": "/portfolio/banheiro/1440p.jpg",
+    "1080p": "/portfolio/banheiro/1080p.jpg",
+    mobile: "/portfolio/banheiro/mobile.jpg",
+    "thumbnail": "/portfolio/banheiro/thumbnail.jpg",
+  },
+  2: {
+    name: "casabranca/anguloalternativo",
+    description: "descrição",
+    "2160p": "/portfolio/casabranca/anguloalternativo/2160p.jpg",
+    "1440p": "/portfolio/casabranca/anguloalternativo/1440p.jpg",
+    "1080p": "/portfolio/casabranca/anguloalternativo/1080p.jpg",
+    mobile: "/portfolio/casabranca/anguloalternativo/mobile.jpg",
+    "thumbnail": "/portfolio/casabranca/anguloalternativo/thumbnail.jpg",
+  },
+  3: {
+    name: "casabranca/dia",
+    description: "descrição",
+    "2160p": "/portfolio/casabranca/dia/2160p.jpg",
+    "1440p": "/portfolio/casabranca/dia/1440p.jpg",
+    "1080p": "/portfolio/casabranca/dia/1080p.jpg",
+    mobile: "",
+    "thumbnail": "/portfolio/casabranca/dia/thumbnail.jpg",
+  },
+  4: {
+    name: "casabranca/dia_wide",
+    description: "descrição",
+    "2160p": "/portfolio/casabranca/dia_wide/2160p.jpg",
+    "1440p": "/portfolio/casabranca/dia_wide/1440p.jpg",
+    "1080p": "/portfolio/casabranca/dia_wide/1080p.jpg",
+    mobile: "/portfolio/casabranca/dia_wide/mobile.jpg",
+    "thumbnail": "/portfolio/casabranca/dia_wide/thumbnail.jpg",
+  },
+  5: {
+    name: "casabranca/noite",
+    description: "descrição",
+    "2160p": "/portfolio/casabranca/noite/2160p.jpg",
+    "1440p": "/portfolio/casabranca/noite/1440p.jpg",
+    "1080p": "/portfolio/casabranca/noite/1080p.jpg",
+    mobile: "",
+    "thumbnail": "/portfolio/casabranca/noite/thumbnail.jpg",
+  },
+  6: {
+    name: "casabranca/noite_wide",
+    description: "descrição",
+    "2160p": "/portfolio/casabranca/noite_wide/2160p.jpg",
+    "1440p": "/portfolio/casabranca/noite_wide/1440p.jpg",
+    "1080p": "/portfolio/casabranca/noite_wide/1080p.jpg",
+    mobile: "/portfolio/casabranca/noite_wide/mobile.jpg",
+    "thumbnail": "/portfolio/casabranca/noite_wide/thumbnail.jpg",
+  },
+  7: {
+    name: "cozinha",
+    description: "descrição",
+    "2160p": "/portfolio/cozinha/2160p.jpg",
+    "1440p": "/portfolio/cozinha/1440p.jpg",
+    "1080p": "/portfolio/cozinha/1080p.jpg",
+    mobile: "/portfolio/cozinha/mobile.jpg",
+    "thumbnail": "/portfolio/cozinha/thumbnail.jpg",
+  },
+  8: {
+    name: "piscina/anguloreto",
+    description: "descrição",
+    "2160p": "/portfolio/piscina/anguloreto/2160p.jpg",
+    "1440p": "/portfolio/piscina/anguloreto/1440p.jpg",
+    "1080p": "/portfolio/piscina/anguloreto/1080p.jpg",
+    mobile: "/portfolio/piscina/anguloreto/mobile.jpg",
+    "thumbnail": "/portfolio/piscina/anguloreto/thumbnail.jpg",
+  },
+  9: {
+    name: "piscina/beirapiscina",
+    description: "descrição",
+    "2160p": "/portfolio/piscina/beirapiscina/2160p.jpg",
+    "1440p": "/portfolio/piscina/beirapiscina/1440p.jpg",
+    "1080p": "/portfolio/piscina/beirapiscina/1080p.jpg",
+    mobile: "/portfolio/piscina/beirapiscina/mobile.jpg",
+    "thumbnail": "/portfolio/piscina/beirapiscina/thumbnail.jpg",
+  },
+  10: {
+    name: "piscina/deck",
+    description: "descrição",
+    "2160p": "/portfolio/piscina/deck/2160p.jpg",
+    "1440p": "/portfolio/piscina/deck/1440p.jpg",
+    "1080p": "/portfolio/piscina/deck/1080p.jpg",
+    mobile: "/portfolio/piscina/deck/mobile.jpg",
+    "thumbnail": "/portfolio/piscina/deck/thumbnail.jpg",
+  },
+  11: {
+    name: "plantas",
+    description: "descrição",
+    "2160p": "/portfolio/plantas/2160p.jpg",
+    "1440p": "/portfolio/plantas/1440p.jpg",
+    "1080p": "/portfolio/plantas/1080p.jpg",
+    mobile: "/portfolio/plantas/mobile.jpg",
+    "thumbnail": "/portfolio/plantas/thumbnail.jpg",
+  },
+  12: {
+    name: "quartodark/angulocadeira",
+    description: "descrição",
+    "2160p": "/portfolio/quartodark/angulocadeira/2160p.jpg",
+    "1440p": "/portfolio/quartodark/angulocadeira/1440p.jpg",
+    "1080p": "/portfolio/quartodark/angulocadeira/1080p.jpg",
+    mobile: "/portfolio/quartodark/angulocadeira/mobile.jpg",
+    "thumbnail": "/portfolio/quartodark/angulocadeira/thumbnail.jpg",
+  },
+  13: {
+    name: "quartodark/anguloquadro",
+    description: "descrição",
+    "2160p": "/portfolio/quartodark/anguloquadro/2160p.jpg",
+    "1440p": "/portfolio/quartodark/anguloquadro/1440p.jpg",
+    "1080p": "/portfolio/quartodark/anguloquadro/1080p.jpg",
+    mobile: "/portfolio/quartodark/anguloquadro/mobile.jpg",
+    "thumbnail": "/portfolio/quartodark/anguloquadro/thumbnail.jpg",
+  },
+  14: {
+    name: "saladeestar/dia",
+    description: "descrição",
+    "2160p": "/portfolio/saladeestar/dia/2160p.jpg",
+    "1440p": "/portfolio/saladeestar/dia/1440p.jpg",
+    "1080p": "/portfolio/saladeestar/dia/1080p.jpg",
+    mobile: "/portfolio/saladeestar/dia/mobile.jpg",
+    "thumbnail": "/portfolio/saladeestar/dia/thumbnail.jpg",
+  },
+  15: {
+    name: "saladeestar/noite",
+    description: "descrição",
+    "2160p": "/portfolio/saladeestar/noite/2160p.jpg",
+    "1440p": "/portfolio/saladeestar/noite/1440p.jpg",
+    "1080p": "/portfolio/saladeestar/noite/1080p.jpg",
+    mobile: "/portfolio/saladeestar/noite/mobile.jpg",
+    "thumbnail": "/portfolio/saladeestar/noite/thumbnail.jpg",
+  },
+  16: {
+    name: "saladeestarjardim/planta",
+    description: "descrição",
+    "2160p": "/portfolio/saladeestarjardim/planta/2160p.jpg",
+    "1440p": "/portfolio/saladeestarjardim/planta/1440p.jpg",
+    "1080p": "/portfolio/saladeestarjardim/planta/1080p.jpg",
+    mobile: "/portfolio/saladeestarjardim/planta/mobile.jpg",
+    "thumbnail": "/portfolio/saladeestarjardim/planta/thumbnail.jpg",
+  },
+  17: {
+    name: "saladeestarjardim/semplanta",
+    description: "descrição",
+    "2160p": "/portfolio/saladeestarjardim/semplanta/2160p.jpg",
+    "1440p": "/portfolio/saladeestarjardim/semplanta/1440p.jpg",
+    "1080p": "/portfolio/saladeestarjardim/semplanta/1080p.jpg",
+    mobile: "/portfolio/saladeestarjardim/semplanta/mobile.jpg",
+    "thumbnail": "/portfolio/saladeestarjardim/semplanta/thumbnail.jpg",
+  },
+  18: {
+    name: "sobradojardim/cima",
+    description: "descrição",
+    "2160p": "/portfolio/sobradojardim/cima/2160p.jpg",
+    "1440p": "/portfolio/sobradojardim/cima/1440p.jpg",
+    "1080p": "/portfolio/sobradojardim/cima/1080p.jpg",
+    mobile: "/portfolio/sobradojardim/cima/mobile.jpg",
+    "thumbnail": "/portfolio/sobradojardim/cima/thumbnail.jpg",
+  },
+  19: {
+    name: "sobradojardim/frente",
+    description: "descrição",
+    "2160p": "/portfolio/sobradojardim/frente/2160p.jpg",
+    "1440p": "/portfolio/sobradojardim/frente/1440p.jpg",
+    "1080p": "/portfolio/sobradojardim/frente/1080p.jpg",
+    mobile: "/portfolio/sobradojardim/frente/mobile.jpg",
+    "thumbnail": "/portfolio/sobradojardim/frente/thumbnail.jpg",
+  },
+}
+
 export const AboutPage = {
   title: "o estúdio",
   paragraphs: {
-    1: `Cupcake ipsum dolor. Sit amet candy canes jelly beans. Halvah icing sesame snaps brownie. Tootsie roll marzipan dragée brownie dessert jelly-o bonbon halvah icing. Bear claw tiramisu candy canes tootsie roll. Tart gingerbread dessert cheesecake lollipop.`,
+    1: `TSmodelagem3d é um estúdio especializado em desenvolvimento de imagens fotorealísticas para Visualização Arquitetônica. Produzindo imagens de Ilustração, Maquetes eletrônicas e animações para o encantar seus clientes e valorizando ainda mais o seu trabalho.
+Nosso estúdio prima para a maior experiência de realização em cada imagem ou animação produzida.  Proporcionando ao cliente uma experiência tão real quanto à experiência em que terão no futuro. Um sonho se transformando em Realidade.
+Um Sonho, Uma Idéia, sua Prévia em nossas mãos.`,
     2: `Jujubes dessert biscuit danish chupa chups icing pastry chupa chups. Topping marzipan pudding carrot cake. Dessert macaroon dragée pastry halvah candy canes chocolate cake. Biscuit marzipan cake tootsie roll. Cheesecake fruitcake cupcake halvah pie. Pie sesame snaps jelly powder gummi bears jujubes chocolate cake`,
   },
 };
@@ -14,88 +191,6 @@ export const PortfolioPage = {
   title: "portfólio",
 };
 
-export type CarouselImage = {
-  name: string;
-  description: string;
-  "2160p": string;
-  "1440p": string;
-  "1080p": string;
-  mobile: string;
-};
-
-export const CarouselImages = {
-  1: {
-    name: "banheiro",
-    description: "descrição",
-    "2160p": "/portfolio/banheiro/2160p.jpg",
-    "1440p": "/portfolio/banheiro/1440p.jpg",
-    "1080p": "/portfolio/banheiro/1080p.jpg",
-    mobile: "/portfolio/banheiro/mobile.jpg",
-  },
-  2: {
-    name: "casabranca",
-    description: "descrição",
-    "2160p": "/portfolio/casabranca/2160p.jpg",
-    "1440p": "/portfolio/casabranca/1440p.jpg",
-    "1080p": "/portfolio/casabranca/1080p.jpg",
-    mobile: "/portfolio/casabranca/mobile.jpg",
-  },
-  3: {
-    name: "emparboreo",
-    description: "descrição",
-    "2160p": "/portfolio/empreendimentoarboreo/2160p.jpg",
-    "1440p": "/portfolio/empreendimentoarboreo/1440p.jpg",
-    "1080p": "/portfolio/empreendimentoarboreo/1080p.jpg",
-    mobile: "/portfolio/empreendimentoarboreo/mobile.jpg",
-  },
-};
-
-export const PortfolioImages = {
-  1: {
-    name: "banheiro",
-    description: "descrição",
-    "2160p": "/portfolio/banheiro/2160p.jpg",
-    "1440p": "/portfolio/banheiro/1440p.jpg",
-    "1080p": "/portfolio/banheiro/1080p.jpg",
-    mobile: "/portfolio/banheiro/mobile.jpg",
-  },
-  2: {
-    name: "casabranca",
-    description: "descrição",
-    "2160p": "/portfolio/casabranca/2160p.jpg",
-    "1440p": "/portfolio/casabranca/1440p.jpg",
-    "1080p": "/portfolio/casabranca/1080p.jpg",
-    mobile: "/portfolio/casabranca/mobile.jpg",
-  },
-  3: {
-    name: "emparboreo",
-    description: "descrição",
-    "2160p": "/portfolio/empreendimentoarboreo/2160p.jpg",
-    "1440p": "/portfolio/empreendimentoarboreo/1440p.jpg",
-    "1080p": "/portfolio/empreendimentoarboreo/1080p.jpg",
-    mobile: "/portfolio/empreendimentoarboreo/mobile.jpg",
-  },
-};
-
-export const AboutImages = {
-  1: {
-    name: "casabranca",
-    description: "descrição",
-    "2160p": "/portfolio/casabranca/2160p.jpg",
-    "1440p": "/portfolio/casabranca/1440p.jpg",
-    "1080p": "/portfolio/casabranca/1080p.jpg",
-    mobile: "/portfolio/casabranca/mobile.jpg",
-  },
-  2: {
-    name: "emparboreo",
-    description: "descrição",
-    "2160p": "/portfolio/empreendimentoarboreo/2160p.jpg",
-    "1440p": "/portfolio/empreendimentoarboreo/1440p.jpg",
-    "1080p": "/portfolio/empreendimentoarboreo/1080p.jpg",
-    mobile: "/portfolio/empreendimentoarboreo/mobile.jpg",
-  },
-};
-
 export const Margin = {
   section: "mb-16",
 };
@@ -103,15 +198,15 @@ export const Margin = {
 export const perfis = [
   {
     id: "architect",
-    label: "Arquiteto(a)",
+    label: "Arquiteto(a)/Engenheiro(a)",
   },
   {
     id: "entrepeneur",
-    label: "Construtora/Empreendedor(a)",
+    label: "Construtora/Incoorporadora",
   },
   {
     id: "designer",
-    label: "Decorador(a)",
+    label: "Designer de Interiores",
   },
 ];
 
@@ -133,3 +228,4 @@ export const projectType = [
     label: "Imagens 360º",
   },
 ];
+
